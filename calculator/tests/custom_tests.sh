35c54e31214afb544a3dc390ca4a5f50248f449c
Abigail VanderPloeg <abigailv@stanford.edu>
2023-02-21 15:55:42 -0800

    fixes to custom tests

--- calculator/tests/custom_tests.sh
+++ calculator/tests/custom_tests.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
+
+# (The absolute path to the program is provided as the first and only argument.)
+CALCULATOR=$1
+
 # Custom Test 01: Ensure program errors with an invalid division input
 if $CALCULATOR 2 / 0; then  # If the return code of $PROGRAM is zero (i.e. success)...
   echo 'ERROR! An invalid run of the application (2 / 0) apparently succeeded?!'
