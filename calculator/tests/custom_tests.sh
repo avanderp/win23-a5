7115de96334ca33b2ff8b2e0da533b06a2f54bf9
Abigail VanderPloeg <abigailv@stanford.edu>
2023-02-21 16:18:05 -0800

    Make custom tests executable

--- calculator/tests/custom_tests.sh
+++ calculator/tests/custom_tests.sh
@@ -3,14 +3,14 @@
 # (The absolute path to the program is provided as the first and only argument.)
 CALCULATOR=$1
 
-# Custom Test 01: Ensure program errors with an invalid division input
-if $CALCULATOR 2 / 0; then  # If the return code of $PROGRAM is zero (i.e. success)...
-  echo 'ERROR! An invalid run of the application (2 / 0) apparently succeeded?!'
+# Custom Test 01: Ensure program errors with an invalid (duplicated) operator
+if $CALCULATOR 2 ** 0; then  # If the return code of $PROGRAM is zero (i.e. success)...
+  echo 'ERROR! An invalid run of the application (2 ** 0) apparently succeeded?!'
   exit 1
 fi
 
 # Custom Test 02: Ensure program succeeds when handling negative number inputs
 if ! $CALCULATOR -2 + 1; then  # If the return code of $PROGRAM is zero (i.e. success)...
-  echo 'ERROR! A valid run of the calculator (1 + 1) failed!'
+  echo 'ERROR! A valid run of the calculator (-2 + 1) failed!'
   exit 1
 fi
